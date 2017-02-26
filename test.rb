c0f63ff0f74661ca79616fbe6bc26def319a65f5
Hayato Yokoi <h-yokoi@sumilab.org>
2017-02-26 16:56:46 +0900

    add comment

--- test.rb
+++ test.rb
@@ -1,11 +1,14 @@
+# input stockpiled games or books and register them
 print "Please input your games or books:";
 STDOUT.flush
 stockpile = gets.to_s.chomp
 
+# add stockpile text
 File.open("stockpile.txt", "a") do |f|
     f.puts "#{stockpile}";
 end
 
+# show stockpile text
 File.open("stockpile.txt", "r") do |f|
     puts f.read
 end
