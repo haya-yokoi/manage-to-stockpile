516e2222ef5fd32d8f0c8486a80b34c40a508c7a
Hayato Yokoi <h-yokoi@sumilab.org>
2017-02-26 21:16:37 +0900

    change the input mechanism

--- test.rb
+++ test.rb
@@ -1,15 +1,19 @@
-# input stockpiled games or books and register them
-print "Please input your games or books:";
-STDOUT.flush
-stockpile = gets.to_s.chomp
 
-# add stockpile text
-File.open("stockpile.txt", "a") do |f|
-    f.puts "#{stockpile}";
+if ARGV[0] == "input"
+    # input stockpiled games or books and register them
+    print "Please input your games or books:";
+    STDOUT.flush
+    stockpile = STDIN.gets.to_s.chomp
+
+    # add stockpile text
+    File.open("stockpile.txt", "a") do |f|
+        f.puts "#{stockpile}";
+    end
+
+    # show stockpile text
+    File.open("stockpile.txt", "r") do |f|
+        puts f.read
+    end
+else
+    print "usage: ruby test.rb [option]\n";
 end
-
-# show stockpile text
-File.open("stockpile.txt", "r") do |f|
-    puts f.read
-end
-
