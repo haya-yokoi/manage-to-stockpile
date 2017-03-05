fad25f395557c42f5c88ca4cb04f1f8616d6cb02
Hayato Yokoi <h-yokoi@sumilab.org>
2017-03-05 15:32:28 +0900

    add clear mechanism

--- test.rb
+++ test.rb
@@ -16,6 +16,28 @@ if ARGV[0] == "input"
     end
 # clear mechanism
 elsif ARGV[0] == "clear"
+    # input cleared games or books and clear them from stockpile text
+    print "Please input your cleared games or books:"
+    STDOUT.flush
+    clear = STDIN.gets.to_s.chomp
+
+    # save the contents of stockpile text at temporary variable
+    buffer = File.open("stockpile.txt", "r") do |f|
+        f.read
+    end
+
+    # replace
+    buffer.gsub!(clear+"\n", "")
+
+    # rewrite
+    File.open("stockpile.txt", "w") do |f|
+        f.puts "#{buffer}";
+    end
+
+    # show stockpile text
+    File.open("stockpile.txt", "r") do |f|
+        puts f.read
+    end
 
 else
     print "usage: ruby test.rb [option]\n";
